c4192e7902f2ca2d46f0f16d58b9bee2c49f46a2
l1nse <gustavoignacio01@gmail.com>
2017-12-05 12:56:08 -0300

    agregar usuario

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,17 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('AppBundle:Home:index.html.twig');
     }
+
+    /**
+     * @Route("/usuarios", name="ver_usuarios")
+     */
+    public function verUsuarios(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:usuarios:usuarios.html.twig');
+    }
+
+
 }
--- src/AppBundle/Controller/UsuariosController.php
+++ src/AppBundle/Controller/UsuariosController.php
@@ -4,8 +4,30 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Usuarios;
 
 class UsuariosController extends Controller
 {
-	
+	/**
+     * @Route("/insert/usuario", name="insert_post")
+     */
+
+	Public function insertPost()
+	{
+		$usuario = new Usuarios();
+		$usuario->setNombre('Prueba1');
+		$usuario->setApellidos('Apellido1');
+		$usuario->setTelefono('56987645261');
+		$usuario->setEdad('23');
+
+		$em = $this->getDoctrine()->getManager();
+
+		$em->persist($usuario);
+
+		$em->flush();
+
+		return new Response('Se inserto nueva entrada ID: '.$usuario->getId());
+
+	}
 }
